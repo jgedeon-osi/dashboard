915aefb464db1eeb2e63b5876ce29fb0bcea448c
Ratnesh Raval <rraval@cambriasolutions.com>
2017-04-24 16:39:14 -0700

    Adding release notes and job aids URL

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,8 @@ class DashboardController < ApplicationController
 
     @intake_url = ENV['INTAKE_URL']
     @cals_url = ENV['CALS_URL']
+
+    @release_notes_url = ENV['RELEASE_NOTES_URL'] || 'https://github.com/ca-cwds/Sandbox/blob/master/Release%20Notes/release_notes.md'
+    @job_aids_url = ENV['JOB_AIDS_URL'] || 'https://cwscms.osi.ca.gov/Portal/Digital-Services-Implementation-Portal/Training?folderId=1957'
   end
 end
