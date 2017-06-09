70f6da42541e2683474f953c4126b438ab5ecb71
Elk Grove <Elk Grove>
2017-06-09 14:26:49 -0700

    deletes token from url after authentication

--- app/controllers/sandbox_base_controller.rb
+++ app/controllers/sandbox_base_controller.rb
@@ -8,12 +8,13 @@ class SandboxBaseController < ApplicationController
 
     # If passed a new token, delete the one from the session
     session.delete(:token) if token
-
     # If no new token and current token exists, continue as normal
     return if session[:token]
 
     if Cwds::Authentication.token_validation(token, ENV['AUTHENTICATION_API_BASE_URL'])
       session[:token] = params[:token]
+      params.delete(:token)
+      redirect_to request.path
     else
       redirect_to Cwds::Authentication.authentication_url(ENV['AUTHENTICATION_API_BASE_URL'], request.url)
     end
