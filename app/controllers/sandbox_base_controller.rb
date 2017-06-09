184b0812b40be7489daef93c6b782a16dee8492f
Elk Grove <Elk Grove>
2017-06-09 14:36:16 -0700

    modified var names for clarification

--- app/controllers/sandbox_base_controller.rb
+++ app/controllers/sandbox_base_controller.rb
@@ -4,15 +4,14 @@ class SandboxBaseController < ApplicationController
   protected
 
   def authenticate_with_cwds
-    token = params[:token]
-
+    new_token = params[:token]
     # If passed a new token, delete the one from the session
-    session.delete(:token) if token
+    session.delete(:token) if new_token
     # If no new token and current token exists, continue as normal
     return if session[:token]
 
-    if Cwds::Authentication.token_validation(token, ENV['AUTHENTICATION_API_BASE_URL'])
-      session[:token] = params[:token]
+    if Cwds::Authentication.token_validation(new_token, ENV['AUTHENTICATION_API_BASE_URL'])
+      session[:token] = new_token
       params.delete(:token)
       redirect_to request.path
     else
